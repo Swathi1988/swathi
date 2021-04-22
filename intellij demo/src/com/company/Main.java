04a66f05ae10015b41ebac6f298bcaeb019f5292
Swathi1988 <82922236+Swathi1988@users.noreply.github.com>
2021-04-22 16:05:12 -0700

    commit addition

---
+++ intellij demo/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//addition of two numbers
+    int a=1,b=2;
+    int c=a+b;
+    System.out.println("c");
+    // c is addition of two numbers
+
+    )
+    }
+}
