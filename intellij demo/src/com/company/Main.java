2581ed96e90e662756a2e3072dbeeba0d9b5b930
Swathi1988 <vadlamudiswathi@gmail.com>
2021-04-22 20:52:40 -0700

    commit

--- intellij demo/src/com/company/Main.java
+++ intellij demo/src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	//addition of two numbers
+	
+    //addition of two numbers
     int a=1,b=2;
     int c=a+b;
     System.out.println("c");
